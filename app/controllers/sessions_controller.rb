89832c5c8bb525582bca8ebb7e906b86fffa5679
Fineax <michal.kanarek@gmail.com>
2020-03-25 19:37:43 +0100

    Create login form add flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,32 @@
 class SessionsController < ApplicationController
 
-
-	def index
-
-	end
-
 	def new
 
 	end
+
+	def create
+		user=User.find_by(username:params[:session][:username])
+		if (user && user.authenticate(params[:session][:password]))
+			session[:user_id]=user.id
+			flash[:info]='You have ben sucessfully logged in'
+			redirect_to root_path
+		else
+			flash.now[:negative]='Incorect password or username '
+			render 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id]= nil
+		redirect_to root_path
+	end
+
+
+	private
+	def session_params
+    	params.require(:session).permit(:username, :password)
+	end
+
+
+
 end
